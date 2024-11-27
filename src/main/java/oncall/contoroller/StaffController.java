cddb56e4edf204180f1b9f2469f3d9dcd4ae5a5f
Teddysir <sdzx0719@naver.com>
2024-11-27 15:55:17 +0900

    feat: 평일, 휴일 비상 근무 순번대로 사원 입력 메서드 구현

--- src/main/java/oncall/contoroller/StaffController.java
+++ src/main/java/oncall/contoroller/StaffController.java
@@ -1,24 +1,56 @@
 package oncall.contoroller;
 
-import camp.nextstep.edu.missionutils.Console;
 import oncall.utils.ServiceValidation;
 import oncall.utils.Splitter;
 import oncall.view.InputView;
 
+import java.util.List;
+
 public class StaffController {
 
     public void start() {
-        inputStartMonthAndDay();
+        startMonthAndDay();
+        employWeekdayStaffs();
+        employHolidayStaffs();
     }
 
-    private void inputStartMonthAndDay() {
+    private void startMonthAndDay() {
         String rawMonthAndDay = InputView.inputMessageMonthAndDay();
-        String[] splitRawMonthAndDay = Splitter.splitCommaDelimiter(rawMonthAndDay);
+
+        ServiceValidation.checkedRawInput(rawMonthAndDay);
+
+        String[] splitRawMonthAndDay = Splitter.getSplitMonthAndDay(rawMonthAndDay);
 
         int startMonth = ServiceValidation.checkedStartMonth(splitRawMonthAndDay[0].trim()); // ex) 12
         ServiceValidation.checkedStartDay(splitRawMonthAndDay[1].trim());
         String startDay = splitRawMonthAndDay[1].trim(); // ex) 월
 
+    }
+
+    private void employWeekdayStaffs() {
+        String rawWeekdayStaffs = InputView.inputMessageWeekdayStaffs();
+
+        ServiceValidation.checkedRawInput(rawWeekdayStaffs);
+
+        List<String> weekdayStaffs = Splitter.getSplitStaffsComma(rawWeekdayStaffs.trim()); // ex) [성훈, 수아, 동균, 효성]
+
+        ServiceValidation.checkedDuplicateNickname(rawWeekdayStaffs, weekdayStaffs);
+        ServiceValidation.checkedNicknameLength(weekdayStaffs);
+        ServiceValidation.checkedMinimumStaffs(weekdayStaffs);
+        ServiceValidation.checkedMaximumStaffs(weekdayStaffs);
+    }
+
+    private void employHolidayStaffs() {
+        String rawHolidayStaffs = InputView.inputMessageHolidayStaffs();
+
+        ServiceValidation.checkedRawInput(rawHolidayStaffs);
+
+        List<String> holidayStaffs = Splitter.getSplitStaffsComma(rawHolidayStaffs.trim());
+
+        ServiceValidation.checkedDuplicateNickname(rawHolidayStaffs, holidayStaffs);
+        ServiceValidation.checkedNicknameLength(holidayStaffs);
+        ServiceValidation.checkedMinimumStaffs(holidayStaffs);
+        ServiceValidation.checkedMaximumStaffs(holidayStaffs);
     }
 
 }
--- src/main/java/oncall/utils/Splitter.java
+++ src/main/java/oncall/utils/Splitter.java
@@ -1,8 +1,17 @@
 package oncall.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Splitter {
 
-    public static String[] splitCommaDelimiter(String rawInput) {
+    public static String[] getSplitMonthAndDay(String rawInput) {
         return rawInput.split(",");
+    }
+
+    public static List<String> getSplitStaffsComma(String rawInput) {
+        return Arrays.stream(rawInput.split(","))
+                .map(String::trim)
+                .toList();
     }
 }
